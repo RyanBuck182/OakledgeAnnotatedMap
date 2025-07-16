7816245fbf5df5d294583edc572c9bfe4bfc9736
ryanbuck182 <ryanbuck182@gmail.com>
2025-07-15 22:58:32 -0400

    Add infowindow for marker

--- src/map.js
+++ src/map.js
@@ -13,7 +13,11 @@ async function initializeMap() {
 
   createMarker({
     title: "Test Marker",
-    position: { lat: 44.454778, lng: -73.227556 }
+    position: { lat: 44.454778, lng: -73.227556 },
+    description: `This is a test decription. Lorem ipsum dolor sit amet
+    consectetur adipisicing elit. Rerum incidunt ex illo reiciendis cupiditate
+    consectetur, aliquid quis, delectus sed quasi ut neque natus aspernatur ab
+    placeat minus nesciunt obcaecati eum.`,
   });
 }
 
@@ -22,6 +26,7 @@ async function initializeMap() {
  * @param {{
  * title: String,
  * position: { lat: Number, lng: Number },
+ * description: String,
  * }} p The parameters.
  * @returns {AdvancedMarkerElement} The marker element.
  */
@@ -29,7 +34,23 @@ function createMarker(p) {
   const marker = new AdvancedMarkerElement({
     map: map,
     position: p.position,
-    title: p.title
+    title: p.title,
+  });
+
+  const infoWindow = new google.maps.InfoWindow({
+    content: `
+      <div style="max-width: 300px;">
+        <h1 id="firstHeading" class="firstHeading">${p.title}</h1>
+        <p>${p.description}</p>
+      </div>
+    `,
+  });
+
+  marker.addListener("click", () => {
+    infoWindow.open({
+      anchor: marker,
+      map: map,
+    });
   });
 
   return marker;
